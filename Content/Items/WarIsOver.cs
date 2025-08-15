9d7f516c87fe4b2fd9271712bc34774bf3b53e35
MagnoliaBreadMan <magnoliamax13@gmail.com>
2025-08-15 10:02:01 -0700

    Add files via upload

---
+++ Content/Items/WarIsOver.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace gunrightsmod.Content.Items
+{
+    public class WarIsOver : ModItem
+    {
+        public override void SetDefaults()
+        {
+            // Modders can use Item.DefaultToRangedWeapon to quickly set many common properties, such as: useTime, useAnimation, useStyle, autoReuse, DamageType, shoot, shootSpeed, useAmmo, and noMelee. These are all shown individually here for teaching purposes.
+
+            // Common Properties
+            Item.width = 20; // Hitbox width of the item.
+            Item.height = 20; // Hitbox height of the item.
+            Item.rare = ItemRarityID.White; // The color that the item's name will be in-game.
+            Item.value = 69;
+            Item.maxStack = 9999;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useAnimation = 15;
+            Item.useTime = 15;
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Paintings.FabsolDeadPlaced>());
+
+        }
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            // Here we add a tooltipline that will later be removed, showcasing how to remove tooltips from an item
+            var line = new TooltipLine(Mod, "Face", "Now we might get Yharim before 2035");
+            tooltips.Add(line);
+
+            line = new TooltipLine(Mod, "Face", "")
+            {
+                OverrideColor = new Color(255, 255, 255)
+            };
+            tooltips.Add(line);
+
+
+
+            // Here we will hide all tooltips whose title end with ':RemoveMe'
+            // One like that is added at the start of this method
+            foreach (var l in tooltips)
+            {
+                if (l.Name.EndsWith(":RemoveMe"))
+                {
+                    l.Hide();
+                }
+            }
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Silk, 1);
+            recipe.AddIngredient(ItemID.AshBlock, 5);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+
+        }
+    }
+}
