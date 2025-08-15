d68c65296f3ffafc482530fb774b86fbc3cb5f3a
MagnoliaBreadMan <magnoliamax13@gmail.com>
2025-08-15 09:57:27 -0700

    Add files via upload

---
+++ Content/Tiles/FabsolDeadPlaced.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace gunrightsmod.Content.Tiles
+{
+    // Simple 3x3 tile that can be placed on a wall
+    public class FabsolDeadPlaced : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.Width = 9;
+            TileObjectData.newTile.Height = 7;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.CoordinateHeights = new[]
+            {
+                16,
+                16,
+                16,
+                16,
+                16,
+                16,
+                16
+            };
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(175, 103, 54), Language.GetText("Custom Painting"));
+            DustType = 91;
+        }
+    }
+}
