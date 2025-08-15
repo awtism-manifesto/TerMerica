02c29fb418bfdeaaed17f36a2a1216bd2591f05e
MagnoliaBreadMan <magnoliamax13@gmail.com>
2025-08-15 08:12:32 -0700

    Add files via upload

---
+++ Content/Items/BottledShimmer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace gunrightsmod.Content.Items
+{
+    public class BottledShimmer : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 20;
+
+            // Dust that will appear in these colors when the item with ItemUseStyleID.DrinkLiquid is used
+            ItemID.Sets.DrinkParticleColors[Type] = new Color[3] {
+                new Color(240, 240, 240),
+                new Color(200, 200, 200),
+                new Color(140, 140, 140)
+            };
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 26;
+            Item.useStyle = ItemUseStyleID.DrinkLiquid;
+            Item.useAnimation = 15;
+            Item.useTime = 15;
+            Item.useTurn = true;
+            Item.UseSound = SoundID.Item3;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.consumable = true;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.buyPrice(copper: 42);
+            Item.buffType = BuffID.Shimmer; // Specify an existing buff to be applied when used.
+            Item.buffTime = 600; // The amount of time the buff declared in Item.buffType will last in ticks. 5400 / 60 is 90, so this buff will last 90 seconds.
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Bottle, 1);
+            recipe.AddCondition(Condition.NearShimmer);
+            recipe.Register();
+        }
+    }
+}
