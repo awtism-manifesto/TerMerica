6a2f042531a0dff19378823f6b1b0006a6113092
MagnoliaBreadMan <magnoliamax13@gmail.com>
2025-08-18 12:31:49 -0700

    Add files via upload

---
+++ Content/Projectiles/WindTomeProjectile.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace gunrightsmod.Content.Projectiles
+{
+    // This example is similar to the Wooden Arrow projectile
+    public class WindTomeProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            // If this arrow would have strong effects (like Holy Arrow pierce), we can make it fire fewer projectiles from Daedalus Stormbow for game balance considerations like this:
+            //ProjectileID.Sets.FiresFewerFromDaedalusStormbow[Type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 30; // The width of projectile hitbox
+            Projectile.height = 30; // The height of projectile hitbox
+
+            Projectile.arrow = false;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.timeLeft = 1200;
+            Projectile.penetrate = 5;
+            Projectile.extraUpdates = 2;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.Dig, Projectile.position); // Plays the basic sound most projectiles make when hitting blocks.
+            for (int i = 0; i < 5; i++) // Creates a splash of dust around the position the projectile dies.
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Cloud);
+                dust.noGravity = true;
+                dust.velocity *= 1.5f;
+                dust.scale *= 0.9f;
+            }
+        }
+    }
+}
