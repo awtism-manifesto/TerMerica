387642e7d16dd70e5668fcfa56c34c7f42696dfd
MagnoliaBreadMan <magnoliamax13@gmail.com>
2025-08-19 07:20:48 -0700

    Add files via upload

---
+++ Content/Tiles/BlankFlagPlaced.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace gunrightsmod.Content.Tiles
+{
+    public class BlankFlagPlaced : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = false;
+            Main.tileSolidTop[Type] = false;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.LavaDeath = true;
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(142, 142, 142), Language.GetText("Flag")); // localized text for "Metal Bar"
+        }
+    }
+}
---
+++ Content/Tiles/SovietFlagPlaced.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace gunrightsmod.Content.Tiles
+{
+    public class SovietFlagPlaced : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = false;
+            Main.tileSolidTop[Type] = false;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.LavaDeath = true;
+            TileObjectData.addTile(Type);
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 18 };
+
+            AddMapEntry(new Color(142, 142, 142), Language.GetText("Flag")); // localized text for "Metal Bar"
+        }
+    }
+}
