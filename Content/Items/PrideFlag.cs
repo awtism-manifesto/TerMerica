7b636e5d2085cdf00e12b929db65d0a44cf27592
MagnoliaBreadMan <magnoliamax13@gmail.com>
2025-08-19 07:22:20 -0700

    Add files via upload

---
+++ Content/Items/PrideFlag.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using MagnoliaMod.Content.Items.Other;
+
+namespace gunrightsmod.Content.Items
+{
+    public class PrideFlag : ModItem
+    {
+        public override void SetDefaults()
+        {
+            // Modders can use Item.DefaultToRangedWeapon to quickly set many common properties, such as: useTime, useAnimation, useStyle, autoReuse, DamageType, shoot, shootSpeed, useAmmo, and noMelee. These are all shown individually here for teaching purposes.
+
+            // Common Properties
+            Item.width = 32; // Hitbox width of the item.
+            Item.height = 32; // Hitbox height of the item.
+            Item.rare = ItemRarityID.White; // The color that the item's name will be in-game.
+            Item.value = 100;
+            Item.maxStack = 9999;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 15;
+            Item.useAnimation = 15;
+            Item.autoReuse = true;
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.PrideFlagPlaced>());
+        }
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            // Here we add a tooltipline that will later be removed, showcasing how to remove tooltips from an item
+            var line = new TooltipLine(Mod, "Face", "");
+            tooltips.Add(line);
+
+            line = new TooltipLine(Mod, "Face", "")
+            {
+                OverrideColor = new Color(255, 255, 255)
+            };
+            tooltips.Add(line);
+
+
+
+            // Here we will hide all tooltips whose title end with ':RemoveMe'
+            // One like that is added at the start of this method
+            foreach (var l in tooltips)
+            {
+                if (l.Name.EndsWith(":RemoveMe"))
+                {
+                    l.Hide();
+                }
+            }
+        }
+
+        // Another method of hiding can be done if you want to hide just one line.
+        // tooltips.FirstOrDefault(x => x.Mod == "ExampleMod" && x.Name == "Verbose:RemoveMe")?.Hide();
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient<Items.BlankFlag>(1);
+            recipe.AddIngredient(ItemID.Wood, 2);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+
+
+
+    }
+}
